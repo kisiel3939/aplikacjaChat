62e855deb7d913c61449b777e5c177b06be0843d
Jerzy Dryszel <jerzydryszel@gmail.com>
2024-12-15 19:10:00 +0100

    optymalizacja kodu oraz wstępna funkcja javascript

---
+++ skryptyIndex.js
@@ -0,0 +1,15 @@
+function rejestracjaKonta() {
+  console.log(1);
+}
+
+function zaloguj() {
+  console.log(2);
+}
+
+//document.getElementById("loguj").style.display="none";
+//document.getElementById("rejestruj").style.display="none";
+//document.getElementById("logowanie").style.display="inherit";
+
+//document.getElementById("loguj").style.display = "none";
+//document.getElementById("rejestruj").style.display = "none";
+//document.getElementById("rejestracja").style.display = "inherit";
